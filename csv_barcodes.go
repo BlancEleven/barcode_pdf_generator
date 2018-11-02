d0501f0edc2d157ae42eac4ac8af999a4e7a1959
Andrew Blanchette <andrew@andrewblanchette.com>
2018-11-02 09:40:38 -0400

    Read CSV file into Student struct

---
+++ csv_barcodes.go
@@ -0,0 +1,54 @@
+package csv_barcode
+
+import (
+	"bufio"
+	"encoding/csv"
+	"io"
+	"log"
+
+	//"encoding/json"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+type Student struct {
+	first string `json:"firstname"`
+	last string `json:"lastname""`
+	pin int `json:"pin"`
+}
+
+func getPin(password string) int {
+	pattern, _ := regexp.Compile("([0-9]+)")
+
+	pinStr := pattern.FindString(password)
+	pin, err := strconv.Atoi(pinStr)
+		if err != nil {
+			fmt.Printf("There was an error extracting the pin: %s", err)
+		}
+	return pin
+}
+
+func ReadFile(filePath string)  {
+	var students []Student
+	csvFile, _ := os.Open(filePath)
+	reader := csv.NewReader(bufio.NewReader(csvFile))
+
+	for {
+		line, err := reader.Read()
+
+		if err == io.EOF {
+			break
+		}else if err != nil{
+			log.Fatal(err)
+		}
+
+		students = append(students, Student{
+			last: line[0],
+			first: line[1],
+			pin: getPin(line[2]),
+		})
+	}
+	fmt.Println(students)
+}
